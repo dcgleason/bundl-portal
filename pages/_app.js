cdc63d17c3e756a1b8e8ef0935c2667c0374606e
Dan Gleason <dannycgleason@gmail.com>
2023-06-14 08:26:32 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ function MyProvider({ children }) {
           <Head>
             <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <title>Bundle Admin Portal</title>
+            <title>Bundl Admin Portal</title>
             <meta />
             <script src="https://apis.google.com/js/api.js"></script>
           </Head>
