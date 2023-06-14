3447b444d53e8c5f721d987a1fe4a02e6af1797d
Dan Gleason <dannycgleason@gmail.com>
2023-06-14 14:09:04 -0400

    update to the context

--- pages/_app.js
+++ pages/_app.js
@@ -12,20 +12,6 @@ export const MyContext = createContext({
     setUserName: () => {},
 });
 
-// _app.js
-import "../css/index.css";
-import Head from "next/head";
-import Layout from "../components/layout";
-import "pure-react-carousel/dist/react-carousel.es.css";
-import { useState, createContext, useContext, useEffect } from "react";
-
-export const MyContext = createContext({
-    userID: null,
-    setUserID: () => {},
-    userName: null,
-    setUserName: () => {},
-});
-
 function MyProvider({ children }) {
     const [userID, setUserID] = useState(null);
 
@@ -77,24 +63,3 @@ function MyApp({ Component, pageProps }) {
 
 export default MyApp;
 
-function MyApp({ Component, pageProps }) {
-    return (
-      <Layout>
-        <div>
-          <Head>
-            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <title>Bundl Admin Portal</title>
-            <link rel="icon" href="/favicon.ico" />
-            <meta />
-            <script src="https://apis.google.com/js/api.js"></script>
-          </Head>
-          <MyProvider>
-            <Component {...pageProps} />
-          </MyProvider>
-        </div>
-      </Layout>
-    );
-}
-
-export default MyApp;
