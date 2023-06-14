2fa52679d8134215a0c768abcdd14ad6ec9cbc6b
Dan Gleason <dannycgleason@gmail.com>
2023-06-14 06:22:21 -0400

    update

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,14 @@
+import { useRouter } from 'next/router';
 import Sidebar from "./sidebar";
 import Header from "./header"
 
 export default function Layout(props) {
+    const router = useRouter();
+
     return (
         <div>
             <div className="flex w-full">
-                <Sidebar />
+                {router.pathname !== '/signup' && router.pathname !== '/signin' && <Sidebar />}
                 <div className="w-full px-6 md:px-10 py-6">
                     <Header />
                     <main>{props.children}</main>
