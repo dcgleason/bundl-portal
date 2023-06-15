65f078916c32c1b89c8cdaa958a89f81186ff8b2
Dan Gleason <dannycgleason@gmail.com>
2023-06-15 10:30:10 -0400

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,11 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 export default NextAuth({
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    providers: [
+      Providers.Google({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
   ],
   callbacks: {
     async jwt(token, user) {
