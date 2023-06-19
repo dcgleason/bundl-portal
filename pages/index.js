121407e3c8fb5a66eca58eb4e4d776df86c5dd00
Dan Gleason <dannycgleason@gmail.com>
2023-06-18 20:12:04 -0400

    update to email send

--- pages/index.js
+++ pages/index.js
@@ -461,34 +461,35 @@ const handleHoverOff = () => {
   };
 
   const handleSendEmail = async () => {
-    if (!session) {
-      // If the user is not signed in, prompt them to do so
-      signIn('google');
-    } else {
-      // If the user is signed in, send the email
-      const recipientEmails = dataSource
-        .filter((student) => student.submitted === "No")
-        .map((student) => student.email);
+    console.log('email sent')
+    // if (!session) {
+    //   // If the user is not signed in, prompt them to do so
+    //   signIn('google');
+    // } else {
+    //   // If the user is signed in, send the email
+    //   const recipientEmails = dataSource
+    //     .filter((student) => student.submitted === "No")
+    //     .map((student) => student.email);
 
-      const response = await fetch('/api/send-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          senderEmail: session.user.email,
-          emailSubject: 'Contribute please - 3 days left!',
-          emailBody: 'We\'d love you to contribute to this bundle',
-          recipientEmails,
-        }),
-      });
+    //   const response = await fetch('https://yay-api.herokuapp.com/email/send', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       senderEmail: session.user.email,
+    //       emailSubject: 'Contribute please - 3 days left!',
+    //       emailBody: 'We\'d love you to contribute to this bundle',
+    //       recipientEmails,
+    //     }),
+    //   });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+    //   if (!response.ok) {
+    //     throw new Error(`HTTP error! status: ${response.status}`);
+    //   }
 
-      console.log('Email sent successfully');
-    }
+    //   console.log('Email sent successfully');
+    // }
   };
   const onEditStudent = (record) => {
     setIsEditing(true);
