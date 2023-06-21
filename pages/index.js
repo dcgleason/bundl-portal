49dea540353f57f22e83fd33036e0030e33d36ac
Dan Gleason <dannycgleason@gmail.com>
2023-06-21 12:28:07 -0400

    updaet

--- pages/index.js
+++ pages/index.js
@@ -201,7 +201,7 @@ const CSV = () => {
               id: index + 1,
               name: value.name || "Name not available",
               email: value.email || "No email given",
-              submitted: value.msg ? "Yes" : "No",
+              submitted: value.msg != '' ? "Yes" : "No",
               notes: '', // Not sure where this data comes from
               submission: value.msg || "No submission",
               picture: !!value.img_file, // Convert to boolean; true if exists, false otherwise
