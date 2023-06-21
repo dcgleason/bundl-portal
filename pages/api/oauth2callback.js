327cdb0e1bc4a35b7be9c23f8798e04627162a76
Dan Gleason <dannycgleason@gmail.com>
2023-06-21 06:58:44 -0400

    update

--- pages/api/oauth2callback.js
+++ pages/api/oauth2callback.js
@@ -24,9 +24,9 @@ export default async function handler(req, res) {
     // Set the tokens in a cookie
     res.setHeader('Set-Cookie', cookie.serialize('auth', JSON.stringify(tokens), {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development', // Use HTTPS in production
+        secure: false, // Use HTTPS in production
         maxAge: 60 * 60 * 24 * 7, // 1 week
-        sameSite: 'lax', // Changed from 'strict' to 'lax'
+        sameSite: 'None', // Changed from 'strict' to 'lax'
         path: '/',
       }));
       
